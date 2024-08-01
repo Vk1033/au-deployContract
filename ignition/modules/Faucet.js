58c7fb1445fc6b1134717791e0da7c3a96c2c940
Vk1033 <66551858+Vk1033@users.noreply.github.com>
2024-08-02 02:01:21 +0530

    Delete ignition/modules directory
    ignition not needed, ether.js deployment

--- ignition/modules/Faucet.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("Faucet", (m) => {
-  const faucet = m.contract("Faucet");
-
-  // m.call(apollo, "launch", []);
-
-  return { faucet };
-});
